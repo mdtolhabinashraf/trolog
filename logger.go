9097071b5476e17b6a541616bda295cd7224cc98
Md. Tolha Bin Ashraf <tolhaofficial0@gmail.com>
2024-10-10 10:18:50 +0600

    Enhance functionality and optimizations for v0.1.3

--- logger.go
+++ logger.go
@@ -25,13 +25,7 @@ const (
 )
 
 var logLevelStrings = [...]string{
-	"DEBU",
-	"INFO",
-	"WARN",
-	"ERRO",
-	"PANI",
-	"TRAC",
-	"UNKNOWN",
+	"DEBU", "INFO", "WARN", "ERRO", "PANI", "TRAC", "UNKNOWN",
 }
 
 var logIDCounter int32 // Using atomic for thread-safe incrementing
@@ -48,28 +42,51 @@ type Logger struct {
 
 var bufferPool = sync.Pool{
 	New: func() interface{} {
-		return &[]byte{} 
+		buf := make([]byte, 0, 512) // Preallocate a buffer with some initial capacity
+		return &buf
 	},
 }
 
-// NewLogger initializes a new logger instance
-func NewLogger(level LogLevel, output io.Writer, colored bool, logFilePath string) (*Logger, error) {
+// Convert string to LogLevel
+func logLevelFromString(levelStr string) LogLevel {
+	switch levelStr {
+	case "debug":
+		return DebugLevel
+	case "info":
+		return InfoLevel
+	case "warn":
+		return WarnLevel
+	case "error":
+		return ErrorLevel
+	case "panic":
+		return PanicLevel
+	case "trace":
+		return TraceLevel
+	default:
+		return InfoLevel
+	}
+}
+
+// NewLogger initializes a new logger instance using string for level
+func NewLogger(levelStr string, output io.Writer, colored bool, logFilePath string) *Logger {
+	level := logLevelFromString(levelStr)
+
 	var logFile *os.File
-	var err error
 	if logFilePath != "" {
+		var err error
 		logFile, err = os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 		if err != nil {
-			return nil, err
+			logFile = nil // Fallback to no file if there is an error
 		}
 	}
 
 	return &Logger{
 		level:   level,
 		output:  output,
-		file:    logFile, // This can be nil if no file is specified
+		file:    logFile,
 		colored: colored,
 		fields:  make(map[string]string),
-	}, nil
+	}
 }
 
 // Close closes the log file if it's being used
@@ -82,16 +99,20 @@ func (l *Logger) Close() error {
 
 // log handles core logging logic and minimizes allocations
 func (l *Logger) log(level LogLevel, message string, extraFields map[string]string) {
-
 	logID := atomic.AddInt32(&logIDCounter, 1)
 
 	buf := bufferPool.Get().(*[]byte)
+	*buf = (*buf)[:0] // Reset the buffer
 	defer bufferPool.Put(buf)
 
-	*buf = (*buf)[:0]
 	timestamp := time.Now().Format(time.RFC3339)
 
-	// Prepare the log message for terminal output (with colors and level)
+	// Prepare the log message with ID first
+	*buf = append(*buf, "ID:"...)
+	*buf = strconv.AppendInt(*buf, int64(logID), 10)
+	*buf = append(*buf, ' ') // Space after ID
+
+	// Prepare the log level and timestamp after ID
 	if level == DebugLevel || level == InfoLevel {
 		*buf = append(*buf, getColor(level)...)
 		*buf = append(*buf, logLevelStrings[level]...)
@@ -102,9 +123,6 @@ func (l *Logger) log(level LogLevel, message string, extraFields map[string]stri
 	}
 	*buf = append(*buf, ' ')
 	*buf = append(*buf, timestamp...)
-	*buf = append(*buf, ' ')
-	*buf = append(*buf, "ID:"...)
-	*buf = append(*buf, strconv.Itoa(int(logID))...)
 	*buf = append(*buf, ' ')
 	*buf = append(*buf, message...)
 
@@ -117,9 +135,7 @@ func (l *Logger) log(level LogLevel, message string, extraFields map[string]stri
 	for key, value := range l.fields {
 		*buf = append(*buf, ' ')
 		*buf = append(*buf, key...)
-		*buf = append(*buf, ':')
-		*buf = append(*buf, ' ')
-		*buf = append(*buf, '"')
+		*buf = append(*buf, ':', ' ', '"')
 		*buf = append(*buf, value...)
 		*buf = append(*buf, '"')
 	}
@@ -127,9 +143,7 @@ func (l *Logger) log(level LogLevel, message string, extraFields map[string]stri
 	for key, value := range extraFields {
 		*buf = append(*buf, ' ')
 		*buf = append(*buf, key...)
-		*buf = append(*buf, ':')
-		*buf = append(*buf, ' ')
-		*buf = append(*buf, '"')
+		*buf = append(*buf, ':', ' ', '"')
 		*buf = append(*buf, value...)
 		*buf = append(*buf, '"')
 	}
@@ -157,6 +171,10 @@ func (l *Logger) log(level LogLevel, message string, extraFields map[string]stri
 // buildLogMessage constructs a log message for writing to file
 func buildLogMessage(level LogLevel, timestamp, message string, fields, extraFields map[string]string, colored bool, logID int32) []byte {
 	var logBuf []byte
+	logBuf = append(logBuf, "ID:"...) // Append ID first
+	logBuf = strconv.AppendInt(logBuf, int64(logID), 10)
+	logBuf = append(logBuf, ' ') // Space after ID
+
 	if colored {
 		logBuf = append(logBuf, getColor(level)...)
 		logBuf = append(logBuf, logLevelStrings[level]...)
@@ -167,9 +185,6 @@ func buildLogMessage(level LogLevel, timestamp, message string, fields, extraFie
 	logBuf = append(logBuf, ' ')
 	logBuf = append(logBuf, timestamp...)
 	logBuf = append(logBuf, ' ')
-	logBuf = append(logBuf, "ID:"...)
-	logBuf = append(logBuf, strconv.Itoa(int(logID))...)
-	logBuf = append(logBuf, ' ')
 	logBuf = append(logBuf, message...)
 
 	if len(fields) > 0 || len(extraFields) > 0 {
@@ -179,9 +194,7 @@ func buildLogMessage(level LogLevel, timestamp, message string, fields, extraFie
 	for key, value := range fields {
 		logBuf = append(logBuf, ' ')
 		logBuf = append(logBuf, key...)
-		logBuf = append(logBuf, ':')
-		logBuf = append(logBuf, ' ')
-		logBuf = append(logBuf, '"')
+		logBuf = append(logBuf, ':', ' ', '"')
 		logBuf = append(logBuf, value...)
 		logBuf = append(logBuf, '"')
 	}
@@ -189,9 +202,7 @@ func buildLogMessage(level LogLevel, timestamp, message string, fields, extraFie
 	for key, value := range extraFields {
 		logBuf = append(logBuf, ' ')
 		logBuf = append(logBuf, key...)
-		logBuf = append(logBuf, ':')
-		logBuf = append(logBuf, ' ')
-		logBuf = append(logBuf, '"')
+		logBuf = append(logBuf, ':', ' ', '"')
 		logBuf = append(logBuf, value...)
 		logBuf = append(logBuf, '"')
 	}
@@ -243,7 +254,7 @@ func valueToString(value interface{}) string {
 	case string:
 		return v
 	case int:
-		return string(intToString(v))
+		return strconv.Itoa(v)
 	case float64:
 		return floatToString(v)
 	case bool:
@@ -264,105 +275,57 @@ func floatToString(f float64) string {
 
 	var result []byte
 	integerPart := int(f)
-	decimalPart := int((f - float64(integerPart)) * 100) 
+	decimalPart := int((f - float64(integerPart)) * 100)
 
-	result = append(result, intToString(integerPart)...)
+	result = append(result, strconv.Itoa(integerPart)...)
 	result = append(result, '.')
 	if decimalPart < 10 {
 		result = append(result, '0')
 	}
-	result = append(result, intToString(decimalPart)...)
+	result = append(result, strconv.Itoa(decimalPart)...)
 
 	return string(result)
 }
 
 // Log methods for different levels
-func (l *Logger) Info(message string) {
-	l.log(InfoLevel, message, nil)
-}
-func (l *Logger) Warn(message string) {
-	l.log(WarnLevel, message, nil)
-}
-func (l *Logger) Error(message string) {
-	l.log(ErrorLevel, message, nil)
-}
-func (l *Logger) Debug(message string) {
-	l.log(DebugLevel, message, nil)
-}
-func (l *Logger) Panic(message string) {
-	l.log(PanicLevel, message, nil)
-}
-func (l *Logger) Trace(message string) {
-	l.log(TraceLevel, message, nil)
-}
+func (l *Logger) Info(message string)  { l.log(InfoLevel, message, nil) }
+func (l *Logger) Warn(message string)  { l.log(WarnLevel, message, nil) }
+func (l *Logger) Error(message string) { l.log(ErrorLevel, message, nil) }
+func (l *Logger) Panic(message string) { l.log(PanicLevel, message, nil) }
+func (l *Logger) Debug(message string) { l.log(DebugLevel, message, nil) }
+func (l *Logger) Trace(message string) { l.log(TraceLevel, message, nil) }
 
-// Log methods for various levels that format messages using a format string and arguments
-func (l *Logger) Infof(format string, args ...interface{}) {
-	l.log(InfoLevel, formatMessage(format, args...), nil)
-}
-func (l *Logger) Warnf(format string, args ...interface{}) {
-	l.log(WarnLevel, formatMessage(format, args...), nil)
-}
-func (l *Logger) Errorf(format string, args ...interface{}) {
-	l.log(ErrorLevel, formatMessage(format, args...), nil)
-}
-func (l *Logger) Debugf(format string, args ...interface{}) {
-	l.log(DebugLevel, formatMessage(format, args...), nil)
-}
-func (l *Logger) Panicf(format string, args ...interface{}) {
-	l.log(PanicLevel, formatMessage(format, args...), nil)
-}
-func (l *Logger) Tracef(format string, args ...interface{}) {
-	l.log(TraceLevel, formatMessage(format, args...), nil)
-}
+// Log methods for different levels
+func (l *Logger) Infof(format string, args ...interface{})  { l.log(InfoLevel, formatMessage(format, args...), nil) }
+func (l *Logger) Debugf(format string, args ...interface{}) { l.log(DebugLevel, formatMessage(format, args...), nil) }
+func (l *Logger) Warnf(format string, args ...interface{})  { l.log(WarnLevel, formatMessage(format, args...), nil) }
+func (l *Logger) Errorf(format string, args ...interface{}) { l.log(ErrorLevel, formatMessage(format, args...), nil) }
+func (l *Logger) Panicf(format string, args ...interface{}) { l.log(PanicLevel, formatMessage(format, args...), nil) }
+func (l *Logger) Tracef(format string, args ...interface{}) { l.log(TraceLevel, formatMessage(format, args...), nil) }
 
-// formatMessage formats a string using placeholders ('%s' for strings and '%d' for integers)
-// by replacing them with corresponding values from args, returning the final formatted string.
+// formatMessage is a custom implementation of string formatting
 func formatMessage(format string, args ...interface{}) string {
-	result := make([]byte, 0, 64)
-	argIndex := 0
-	for i := 0; i < len(format); i++ {
-		if format[i] == '%' && i+1 < len(format) {
-			nextChar := format[i+1]
-			switch nextChar {
-			case 's':
-				result = append(result, []byte(args[argIndex].(string))...)
-				argIndex++
-			case 'd':
-				result = append(result, intToString(args[argIndex].(int))...)
-				argIndex++
-			}
-			i++
-		} else {
-			result = append(result, format[i])
-		}
-	}
-	return string(result)
-}
-
-// intToString converts an integer to a string.
-func intToString(i int) []byte {
-	if i == 0 {
-		return []byte{'0'}
-	}
-	var buf [10]byte
-	n := 0
-	negative := false
-	if i < 0 {
-		negative = true
-		i = -i
-	}
-	for i > 0 {
-		buf[n] = byte('0' + i%10)
-		i /= 10
-		n++
-	}
-	if negative {
-		buf[n] = '-'
-		n++
-	}
-	for j := 0; j < n/2; j++ {
-		buf[j], buf[n-j-1] = buf[n-j-1], buf[j]
-	}
-	return buf[:n]
+    var result string
+    argIndex := 0
+    for i := 0; i < len(format); i++ {
+        if format[i] == '%' && i+1 < len(format) {
+            switch format[i+1] {
+            case 's':
+                result += valueToString(args[argIndex])
+                argIndex++
+                i++ // Skip the format specifier
+            case 'd':
+                result += strconv.Itoa(args[argIndex].(int))
+                argIndex++
+                i++ // Skip the format specifier
+            case 'f':
+                result += floatToString(args[argIndex].(float64))
+                argIndex++
+                i++ // Skip the format specifier
+            }
+        } else {
+            result += string(format[i])
+        }
+    }
+    return result
 }
